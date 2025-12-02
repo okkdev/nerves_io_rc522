1841d3acd6ca31c12e9ac0236d1af5e3c0920018
Tobias Zischler <tobias.zischler@gmx.ch>
2025-12-02 11:15:58 +0100

    change main loop to send

--- src/main.c
+++ src/main.c
@@ -18,103 +18,112 @@
 
 #include <ei.h>
 
-#define err(code, msg) (fprintf(stderr, msg "\n"),exit(code));
+#define err(code, msg) (fprintf(stderr, msg "\n"), exit(code));
 #define dbg(msg) (fprintf(stderr, msg "\n"));
 
 void erlcmd_send(char *response, size_t len);
 uint8_t spi_init(uint32_t spi_speed);
 void send_tag(const char *uid, size_t len);
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
+    uint8_t SN[10];
+    uint16_t CType = 0;
+    uint8_t SN_len = 0;
+    char status;
+    int tmp;
 
-	uint8_t SN[10];
-	uint16_t CType = 0;
-	uint8_t SN_len = 0;
-	char status;
-	int tmp;
+    char *p;
+    char sn_str[23];
 
-	char *p;
-	char sn_str[23];
+    uint32_t spi_speed = 10000000L;
 
-	uint32_t spi_speed = 10000000L;
-
-
-	if (argc != 2) {
+    if (argc != 2)
+    {
         err(1, "Usage: rc522 <spi_speed|test>");
     }
 
     // test mode; send tag to host every second
-    if (!strcmp(argv[1], "test")) {
+    if (!strcmp(argv[1], "test"))
+    {
         dbg("RC522 port test mode.");
-        for (;;) {
+        for (;;)
+        {
             send_tag("foo", 3);
             usleep(1000000);
         }
     }
 
-    spi_speed = (uint32_t)strtoul(argv[1],NULL,10);
-    if (spi_speed>125000L) spi_speed=125000L;
-    if (spi_speed<4) spi_speed=4;
+    spi_speed = (uint32_t)strtoul(argv[1], NULL, 10);
+    if (spi_speed > 125000L)
+        spi_speed = 125000L;
+    if (spi_speed < 4)
+        spi_speed = 4;
 
-	if (spi_init(spi_speed)) {
+    if (spi_init(spi_speed))
+    {
         err(1, "SPI initialization failed.");
     }
 
-	InitRc522();
+    InitRc522();
 
-	for (;;) {
-		status= find_tag(&CType);
-		if (status == TAG_NOTAG) {
-			usleep(50000);
-			continue;
-		} else if ((status!=TAG_OK) && (status!=TAG_COLLISION)) {
+    for (;;)
+    {
+        status = find_tag(&CType);
+        if ((status != TAG_OK) && (status != TAG_COLLISION))
+        {
             continue;
         }
 
-		if (select_tag_sn(SN,&SN_len) != TAG_OK) {
+        if (select_tag_sn(SN, &SN_len) != TAG_OK)
+        {
             continue;
         }
 
-		p = sn_str;
-		for (tmp=0;tmp<SN_len;tmp++) {
-			sprintf(p,"%02X",SN[tmp]);
-			p+=2;
-		}
+        p = sn_str;
+        for (tmp = 0; tmp < SN_len; tmp++)
+        {
+            sprintf(p, "%02X", SN[tmp]);
+            p += 2;
+        }
         *p = 0;
 
-        fprintf(stderr,"Type: %04X, Serial: %s\n",CType,&sn_str[1]);
+        fprintf(stderr, "Type: %04X, Serial: %s\n", CType, &sn_str[1]);
         send_tag(sn_str, 2 * SN_len);
 
-		PcdHalt();
-	}
+        PcdHalt();
 
-	bcm2835_spi_end();
-	bcm2835_close();
-	return 0;
+        usleep(100000); // sleep for 100ms and then reread it. Handling of same tag multiple times is done on erlang side.
+    }
 
+    bcm2835_spi_end();
+    bcm2835_close();
+    return 0;
 }
 
+uint8_t spi_init(uint32_t spi_speed)
+{
+    uint16_t sp;
 
-uint8_t spi_init(uint32_t spi_speed) {
-	uint16_t sp;
+    sp = (uint16_t)(250000L / spi_speed);
+    if (!bcm2835_init())
+    {
+        dbg("Can't init BCM2835!");
+        return 1;
+    }
 
-	sp=(uint16_t)(250000L/spi_speed);
-	if (!bcm2835_init()) {
-		dbg("Can't init BCM2835!");
-		return 1;
-	}
-
-	bcm2835_spi_begin();
-	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
-	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-	bcm2835_spi_setClockDivider(sp); // The default
-	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
-	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
-	return 0;
+    bcm2835_spi_begin();
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST); // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);              // The default
+    bcm2835_spi_setClockDivider(sp);                         // The default
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                 // The default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW); // the default
+    return 0;
 }
 
-void send_tag(const char *uid, size_t len) {
+void send_tag(const char *uid, size_t len)
+{
     char resp[1024];
     int resp_index = sizeof(uint16_t); // Space for payload size
     ei_encode_version(resp, &resp_index);
@@ -137,13 +146,15 @@ void erlcmd_send(char *response, size_t len)
     memcpy(response, &be_len, sizeof(be_len));
 
     size_t wrote = 0;
-    do {
+    do
+    {
         ssize_t amount_written = write(STDOUT_FILENO, response + wrote, len - wrote);
-        if (amount_written < 0) {
+        if (amount_written < 0)
+        {
             if (errno == EINTR)
                 continue;
 
-            //err(EXIT_FAILURE, "write");
+            // err(EXIT_FAILURE, "write");
             exit(0);
         }
 
