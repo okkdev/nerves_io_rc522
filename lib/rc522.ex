22a45044c10b748f743a880b1fb3d65a25406a3a
Tobias Zischler <tobias.zischler@gmx.ch>
2026-01-23 14:32:47 +0100

    change irq setup to write 0x7F instead of 0x80

--- lib/rc522.ex
+++ lib/rc522.ex
@@ -392,7 +392,7 @@ defmodule RC522Elixir do
 
     # Set up registers
     write_reg(ctx, com_ien_reg, irq_en ||| 0x80)
-    clear_bit_mask(ctx, com_irq_reg, 0x80)
+    write_reg(ctx, com_irq_reg, 0x7F)
     set_bit_mask(ctx, fifo_level_reg, 0x80)
     write_reg(ctx, command_reg, @pcd_idle)
 
